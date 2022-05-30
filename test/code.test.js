5d51fcb1dfd7f71d0881ebfd822d38ec2e3097fc
MDices <lgs.mttp@gmail.com>
2022-05-30 10:37:20 -0300

    update no test

--- test/code.test.js
+++ test/code.test.js
@@ -5,7 +5,7 @@
 
 const { sum } = require("./code.js");
 
-describe("test functions", () => {
+describe("test functions sum", () => {
   it("sums 2 numbers", () => {
     expect(sum(1, 2)).toBe(3);
   });
